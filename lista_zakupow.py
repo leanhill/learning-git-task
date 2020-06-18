21c72a20a82c07f1fb98a6db93687c99f2faed11
Andrzej Kaniewski <andrzej.kaniewski@gmail.com>
2020-06-19 00:34:43 +0200

    Print with capitalized first letters

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -3,5 +3,9 @@ lista_zakupów={
     "warzywniak":["marchew", "seler","rukola"]
 }
 for i, a in lista_zakupów.items():
-        print("Idę do %s"%i,"i kupuję tu następujące rzeczy:%s"%a)
+    for b in range (len(a)):
+        a[b] = a[b].capitalize()
+    i = i.capitalize()
+    print("Idę do %s"%i,"i kupuję tu następujące rzeczy:%s"%a)
+
 
