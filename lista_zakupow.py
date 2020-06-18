f81978a2e93bd4610f81418d8d2c9fdd6f165a1a
Andrzej Kaniewski <andrzej.kaniewski@gmail.com>
2020-06-19 00:32:52 +0200

    Print content of dictionary

--- lista_zakupow.py
+++ lista_zakupow.py
@@ -2,3 +2,6 @@ lista_zakupów={
     "piekarnia":["chleb", "pączek","bułki"],
     "warzywniak":["marchew", "seler","rukola"]
 }
+for i, a in lista_zakupów.items():
+        print("Idę do %s"%i,"i kupuję tu następujące rzeczy:%s"%a)
+
